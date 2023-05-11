ae30ebe71f5ab032edc6045bcd3798c14e617fce
angel <ANGEL.GARCIA0038@GMAIL.COM>
2023-05-11 13:17:40 -0700

    added the time to the top of the page. Also added a save button function on click . So far it only grabs the description that the user wrote and the time block it is written on

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+currentDay = dayjs();
+$('#currentDay').text(currentDay)
+
+
+
 $(function () {
+
+$(".saveBtn").on('click', function () {
+    let description = $(this).siblings(".description").val();
+    let timeBlock = $(this).parent(".time-block").attr("id")
+    console.log(description)
+    console.log(timeBlock)
+})
+
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -14,6 +28,8 @@ $(function () {
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
+
+
     //
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
