726cb9c39b4e0c64261f6b359b5c90761ee15fb6
angel <ANGEL.GARCIA0038@GMAIL.COM>
2023-05-13 13:56:16 -0700

    added a function that makes the description entered by the user save after the page is refreshed.

--- script.js
+++ script.js
@@ -8,15 +8,27 @@ $('#currentDay').text(currentDay)
 
 $(function () {
 
-$(".saveBtn").on('click', function () {
+$(".saveBtn").on('click', function (event) {
     // the description is grabbing the description written by the user
     // the timeblock is grabbing which timeblock the user is writing on or which one is saved
-
+    event.preventDefault();
     let description = $(this).siblings(".description").val();
-    let timeBlock = $(this).parent(".time-block").attr("id")
+    let timeBlock = $(this).parent().attr("id").split("-")[1]
     console.log(description)
     console.log(timeBlock)
-    localStorage.setItem(description, timeBlock)
+    localStorage.setItem(timeBlock, description) 
+})
+//The loop goes through all time blocks and gets all the descriptions from the local storage
+
+$(document).ready(function(){
+    for(let i = 9; i <= 17; i++) {
+// getting the description from local storage based on the numeber of the id  
+        let description = localStorage.getItem(i);
+        // if there is a description found val is called on the description textarea to set the value.
+        if (description) {
+            $("#hour-" + i + " .description").val(description)
+        }
+    }
 })
 // Function to set color depending on time of the day
 function timeColor(){
@@ -48,6 +60,10 @@ else {
 
 }
 timeColor()
+
+
+$("#hour-09 .time-block").val(localStorage.getItem("09"))
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
